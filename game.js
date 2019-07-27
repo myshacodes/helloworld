23763f6f1e37511c9aef34b4b40277e2073660e4
myshacodes <52944820+myshacodes@users.noreply.github.com>
2019-07-27 15:27:22 -0500

    I finished my game!!!

--- game.js
+++ game.js
@@ -1,25 +1,37 @@
+var loops = 0;
+var charactersVisible = false;
+var gameScore = 0;
+
 function startGame() {
   gameLoop();
 }
-var loops = 0;
-var charactersVisible = false;
+
 function gameLoop() {
   charactersVisible = !charactersVisible;
+  createCharacters();
   loops++;
   if (loops < 12) {
-    setTimeout(gameLoop, 3000);
+    setTimeout(gameLoop, charactersVisible ? 1000 : 3000);
   } else {
     alert("Game Over!");
+    alert("You're score is " + gameScore);
   }
 }
-function flashCharacters() {
+
+function createCharacters() {
   var board = document.getElementById("board");
-  var classToSet = "";
   var classToSet = charactersVisible ? "character visible" : "character hidden";
+  for (var index = 0; index < 6; index++) {
+    board.children[index].className = classToSet;
+    board.children[index].innerHTML = "";
+    board.children[index].onclick = function() {
+      gameScore += -2; // gameScore = gameScore + 2;
+    };
+  }
+  var randomNumber = Math.floor(Math.random() * 6) + 1;
+  board.children[randomNumber - 1].innerHTML = "";
+  board.children[randomNumber - 1].className = classToSet + " thief";
+  board.children[randomNumber - 1].onclick = function() {
+    gameScore += 2;
+  };
 }
-for (var index = 0; index < 6; index++) {
-  board.children[index].className = classToSet;
-}
-var loops = 0;
-var people = false;
-var gameScore = 0;
